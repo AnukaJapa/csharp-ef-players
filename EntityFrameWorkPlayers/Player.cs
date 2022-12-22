4daa0cd73115c649a0df1db60ca035571540803a
AnukaJapa <japaridze.ana96@gmail.com>
2022-12-22 15:26:15 +0100

    Add project files.

---
+++ EntityFrameWorkPlayers/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkPlayers
+{
+    [Table("player")]
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+        [Required]
+        public string NomePlayer { get; set; }
+        [Required]
+        public string CognomePlayer { get; set; }
+        public int Punteggio { get; set; }
+        public int NumeroPartiteGiocate { get; set; }
+        public int NumeroPartiteVinte { get; set; }
+
+    }
+}
---
+++ EntityFrameWorkPlayers/SportContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkPlayers
+{
+    public class SportContext:DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=PlayersDB;" +
+            "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
